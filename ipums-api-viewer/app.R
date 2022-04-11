591a75188f1801db177a4908303d464aa923b90a
Dan Ehrlich <ehrli097@umn.edu>
2022-04-11 08:52:20 -0500

    adding previous examples

--- ipums-api-viewer/app.R
+++ ipums-api-viewer/app.R
@@ -42,31 +42,44 @@ if(sum(api_check)>1 | sum (data_check) > 1){
   warning("Error: only copy each allowed in shiny folder: extract definitions (.json), ddi (.xml), or data file (.dat.gz)")
 }
 
+data_check <- list.files() %>% str_detect(".xml")
+extract_path <- list.files(pattern = ".json")
 
-if(!any(data_check)){
-  tt <- define_extract_from_json(extract_path, "usa") %>% submit_extract() %>% wait_for_extract() %>% download_extract()
+api_check <- list.files() %>% str_detect(".json")
+
+
+if(sum(api_check)>1 | sum (data_check) > 1){
+  warning("Error: only copy each allowed in shiny folder: extract definitions (.json), ddi (.xml), or data file (.dat.gz)")
 }
 
-ddi_path <- list.files(pattern = ".xml")
-
-ddi <- read_ipums_ddi(ddi_path)
-data <- read_ipums_micro(ddi)
 
 
+if (!file.exists("prcs_migration_extract.xml")) {
+  # Load extract definition from JSON
+  prcs_migration_extract <- define_extract_from_json(
+    "prcs_migration_extract.json",
+    "usa"
+  )
+  # Submit, wait for, and download extract
+  ddi_filename <- submit_extract(prcs_migration_extract) %>% 
+    wait_for_extract() %>% 
+    download_extract() %>% 
+    basename()
+  # Infer data file name from DDI file name
+  data_filename <- str_replace(ddi_filename, "\\.xml$", ".dat.gz")
+  # Standardize DDI and data file names #<<
+  file.rename(ddi_filename, "prcs_migration_extract.xml") #<<
+  file.rename(data_filename, "prcs_migration_extract.dat.gz") #<<
+}
 
-#### perform educ recode from pres
-# 
-# college_regex <- "^[123] year(s)? of college$"
-# data$EDUCD3 <- data$EDUCD %>%
-#   lbl_collapse(~.val %/% 10) %>% 
-#   lbl_relabel(
-#     lbl(2, "Less than High School") ~.val > 0 & .val < 6,
-#     lbl(3, "High school") ~.lbl == "Grade 12",
-#     lbl(4, "Some college") ~str_detect(.lbl, college_regex),
-#     lbl(5, "College or more") ~.val %in% c(10, 11)
-#   ) %>%
-#   as_factor()
+ddi <- read_ipums_ddi("prcs_migration_extract.xml")
+data <- read_ipums_micro(
+  ddi,
+  data_file = "prcs_migration_extract.dat.gz"
+)
 
+
+#### Initital Prep ####
 
 select_choices <- data.frame("Var"= ddi$var_info$var_name)
 
@@ -82,8 +95,73 @@ vals <- NULL
 
 
 
+#### Migration prep ####
 
-##### UI #####
+mig_data <- data
+
+college_regex <- "^[123] year(s)? of college$"
+mig_data <- mig_data %>% 
+  mutate(
+    EDUCD3 = EDUCD %>%
+      lbl_collapse(~.val %/% 10) %>% 
+      lbl_relabel(
+        lbl(2, "Less than High School") ~.val > 0 & .val < 6,
+        lbl(3, "High school") ~.lbl == "Grade 12", #<<
+        lbl(4, "Some college") ~str_detect(.lbl, college_regex), #<<
+        lbl(5, "College or more") ~.val %in% c(10, 11)
+      ) %>%
+      as_factor()
+  )
+
+# Prep income variable
+value_to_quintile <- function(x) {
+  cut_points <- quantile(x, probs = c(0.2, 0.4, 0.6, 0.8), na.rm = TRUE)
+  cut(
+    x, 
+    breaks = c(-Inf, cut_points, Inf), 
+    labels = c("Lowest", "Lower Middle", "Middle", "Upper Middle", "Highest"),
+    ordered_result = TRUE
+  )
+}
+
+hhincome_quintiles <- mig_data %>% 
+  filter(PERNUM == 1 & HHINCOME != 9999999) %>% 
+  group_by(YEAR) %>% 
+  mutate(hhincome_quintile = value_to_quintile(HHINCOME)) %>% 
+  ungroup() %>% 
+  select(YEAR, SERIAL, hhincome_quintile)
+
+mig_data <- mig_data %>% 
+  left_join(hhincome_quintiles, by = c("YEAR", "SERIAL"))
+
+# Prep migration variable
+mig_data <- mig_data %>% 
+  mutate(
+    moved_in_last_year = case_when(
+      MIGRATE1 %in% c(0, 9) ~ NA, 
+      MIGRATE1 == 1 ~ FALSE, 
+      MIGRATE1 %in% 2:4 ~ TRUE
+    )
+  )
+
+
+# Prep age variable
+age_to_age_group <- function(x) {
+  cut_points <- c(9, 17, 34, 64)
+  cut(
+    x,
+    breaks = c(-Inf, cut_points, Inf),
+    labels = c("0-9", "10-17", "18-34", "35-64", "65+"),
+    ordered_result = TRUE
+  )
+}
+
+mig_data <- mig_data %>% 
+  mutate(age_group = age_to_age_group(AGE))
+
+
+
+# UI #####
 
 ui <- fluidPage(
     # Application title
@@ -113,7 +191,7 @@ ui <- fluidPage(
         mainPanel(
           tabsetPanel(
           tabPanel(
-          "Plot",
+          "Interactive Plot",
           h3(textOutput("main_plot_lbl")),
           plotOutput("main_plot"),
           h3(textOutput("main_tab_lbl")),
@@ -126,6 +204,19 @@ ui <- fluidPage(
           textOutput("var_desc"),
           h3("Value Labels (All Possible)"),
           tableOutput("vals_lbls")
+        ),
+        tabPanel(
+          "Migration Ex.",
+          tabsetPanel(
+            tabPanel("Overall",
+                     plotOutput("mig_plot_all")),
+            tabPanel("By Education",
+                     plotOutput("mig_plot_edu")),
+            tabPanel("By HH Income",
+                     plotOutput("mig_plot_inc")),
+            tabPanel("By Age",
+                     plotOutput("mig_plot_age"))
+          )
         )
         
         
@@ -251,7 +342,7 @@ server <- function(input, output) {
 
     })
 
-
+  #### Main table ####
 
 
     output$main_tab <- renderTable({
@@ -291,15 +382,113 @@ server <- function(input, output) {
         )
         par("mar" = c(0,0,0,0))
         plot(1, type = "n", bty = "n", xaxt = "n", yaxt="n", xlab = "", ylab = "")
-        legend("center", legend = rownames(to_plot()), pch = 22, pt.bg = cols, cex = 1.5)
-        
-        
-      }
-
-
-
-        
+        legend("center", legend = rownames(to_plot()), pch = 22, pt.bg = cols, cex = 1.5) 
+      }  
     })
+    
+    #### Render text for migration example
+    # Migration 2015-2019 {.tabset}
+    
+  output$mig_text <- renderText(
+    "The percentage of people who had moved in the last year increased between 2017 
+    and 2018 from about 6% to over 8% among all persons in Puerto Rico, but the 
+    magnitude of this trend varies by education, household income, and age.
+    
+    Note: These graphs show trends in point estimates from sample data, without 
+    displaying estimates of sampling error. Differences over time or across groups 
+    may not be statistically significant. To calculate confidence intervals for 
+    point estimates, follow the 
+    [IPUMS USA instructions for using replicate weights](https://usa.ipums.org/usa/repwt.shtml)."
+  )
+    
+    #### Render migration plots ####
+    
+    
+    
+    ## Overall
+    
+  output$mig_plot_all <- renderPlot({
+    
+
+    # ```{r migration-graph-1, dpi=300, fig.height=5, fig.width=8, echo = FALSE}
+    mig_data %>% 
+      group_by(YEAR) %>% 
+      summarize(
+        pct_moved = 100 * sum(PERWT[moved_in_last_year]) / sum(PERWT)
+      ) %>% 
+      ggplot(aes(x = YEAR, y = pct_moved)) +
+      geom_line() +
+      labs(
+        title = "Percentage of people who moved in the past year, 2015-2019",
+        x = NULL,
+        y = "%"
+      )
+    
+  })
+    ## By education
+    
+  output$mig_plot_edu <- renderPlot({
+    mig_data %>% 
+      filter(AGE >= 25) %>%
+      group_by(YEAR, EDUCD3) %>% 
+      summarize(
+        pct_moved = 100 * sum(PERWT[moved_in_last_year]) / sum(PERWT),
+        .groups = "drop"
+      ) %>% 
+      ggplot(aes(x = YEAR, y = pct_moved)) +
+      geom_line() +
+      facet_wrap(~EDUCD3) +
+      labs(
+        title = "Percentage of people who moved in the past year, 2015-2019",
+        subtitle = "Among persons age 25 and older",
+        x = NULL,
+        y = "%"
+      )
+  })
+    
+    ## By household income
+    
+  output$mig_plot_inc <- renderPlot({
+    mig_data %>% 
+      filter(!is.na(hhincome_quintile)) %>% 
+      group_by(YEAR, hhincome_quintile) %>% 
+      summarize(
+        pct_moved = 100 * sum(PERWT[moved_in_last_year]) / sum(PERWT),
+        .groups = "drop"
+      ) %>% 
+      ggplot(aes(x = YEAR, y = pct_moved)) +
+      geom_line() +
+      facet_wrap(~hhincome_quintile) +
+      labs(
+        title = "Percentage of people who moved in the past year, 2015-2019",
+        x = NULL,
+        y = "%"
+      )
+    
+  })
+    
+    
+    
+    ## By age
+   
+  renderPlot({
+    mig_data %>% 
+      group_by(YEAR, age_group) %>% 
+      summarize(
+        pct_moved = 100 * sum(PERWT[moved_in_last_year]) / sum(PERWT),
+        .groups = "drop"
+      ) %>% 
+      ggplot(aes(x = YEAR, y = pct_moved)) +
+      geom_line() +
+      facet_wrap(~age_group) +
+      labs(
+        title = "Percentage of people who moved in the past year, 2015-2019",
+        x = NULL,
+        y = "%"
+      )
+    })
+    
+    
 }
 
 # Run the application
