90a45ae1f7c4f58afd4684a6ff019f5d3ebe8bbf
Dan Ehrlich <ehrli097@umn.edu>
2022-04-22 08:54:30 -0500

    adding launch script

---
+++ launch_app.R
@@ -0,0 +1,12 @@
+## Launch App
+
+## To launch shiny app:
+
+## CLICK "Run" in the top-right of this window. Or
+
+## PRESS CTRL + ENTER to run the following code:
+
+shiny::runApp("ipums-api-viewer")
+
+
+## ALTERNATIVLY: You can run type the above code, specifying the sub-directory that contains the app directly from console. OR you could open the script located in "ipums-api-viewer/app.R" and click "Run App"
